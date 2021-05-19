45cbb237bdc0d2a7cb5486bda16d526264f7dfe6
ValwareIRC <v.a.pond@outlook.com>
2021-05-19 15:19:20 +0100

    inc version

--- example.config.php
+++ example.config.php
@@ -66,56 +66,6 @@ $cf = [
 	/* Name of the database within the SQL server */
 	'sqldb' => 'sql_db_name',
 
-	/* Table name where the users can be found */
-	'sql_users' => 'users',
-
-	/* Table name where the users metadata can be found */
-	'sql_usermeta'=> 'usermeta',
-
-	/* Table where the users capabilities can be found */
-	'sql_clicap' => 'capabilities',
-	
-	/* OTHER settings */
-	
-	/* Anope SQL table prefix */
-	//'anopetable' => 'anope_',
-	
-	/* WordPress SQL table prefix */
-	//'wp-prefix' => 'wp_',
-	
-	/*	UnrealIRCd SQL table prefix
-		Uncomment this line if you have SQL set up correctly and want SQL stats from
-		your UnrealIRCd server.
-		
-		Check out src/unrealircd for more details
-	*/
-	//'unrealtable' => 'unreal_',
-	
-	
-	/* modes for GateWay to set on connect */
-	'modes_on_connect' => '+iwx',
-	
-	/* modes for GateWay to set on oper */	
-	'modes_on_oper' => '+iwx',
-
-	/* Minimum amount of days a person must be registered before using a third party client */
-	/* Comment out this line if you want there to be no minumum (disable the requirement altogether) */
-	'thirdparty_min_reg_days' => '30',
-
-
-	/* The server which allows third-party connections */
-	'thirdparty_server' => 'third.pornhub.com',
-	
-	
-	/* Minimum allowed age of chatters */
-	'min_age' => '18',
-	
-	/* Maximum age of chatters */
-	//'max_age' => '110', /* Commented out because we don't want max age but there just in case :> */
-	
-	
-	// Listem mechanism - How does GateWay ask for the channels to join? 'server' or 'chanserv'  or 'sql'
-	'listmech' => 'sql',
 
 	/* Important chans yo. These are the chans that GateWay will try to join before all the others. */
 	
@@ -125,30 +75,7 @@ $cf = [
 	'helpchan' => '#help', /* The Help room */
 	
 
-	/* Not imperative */
-	/* Landing room for everyone */
-	'landingchan' => '#lounge',
-
-
-	/* Force join users to their location rooms when they connect */
-	'fjoin_loc_chans' => 'yes',
-
-	/* Force join users to their age-respective rooms when they connect */
-	'fjoin_age_chans' => 'no',
-
-
 	
-	/* Set mode on staff when they connect */
-	'staffmode_connect' => '+qh-R',
-
-	/* Force join rooms for staff, separated by a "," */
-	'staffchan_connect' => '#staff,#help',
-	
-
-
-	/* If a user tries to connect to chat who is banned according to Profile Grid */
-	'userban_connect_msg' => 'Your account is disabled. For more information, please email appeals@pornhub.com',
-
 ]
 
 
