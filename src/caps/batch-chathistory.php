7e7b9cdd72dc7de4a73ea79a4767cf85c5341f17
ValwareIRC <v.a.pond@outlook.com>
2021-05-26 02:14:14 +0100

    woooooooops

--- src/caps/batch-chathistory.php
+++ src/caps/batch-chathistory.php
@@ -9,20 +9,18 @@ hook::func("batch", function($u){
 	$batchtarget = $parv[2] ?? NULL;
 	$batchkey = $parv[0];
 	
-	/*if ($batchkey[0] == "+"){
+	if ($batchkey[0] == "+"){
 		
 		$batchkey = ltrim($batchkey,"-");
 		
 		$batch[$batchtarget] = $batchkey;
 		
 		$reversebatch[$batch[$batchtarget]] = $batchtarget;
-		$gw->msg("#Valeyard","Added batch: $batch[$batchtarget]");
 	}
-	/*elseif ($batchkey[0] == "-"){
+	elseif ($batchkey[0] == "-"){
 		$batchkey =  ltrim($batchkey,"-");
 		$batch[$batchkey] = NULL;
-		$gw->msg("#Valeyard","Deleted batch: $batchkey");
-	}*/
+	}
 	return;
 });
 
@@ -53,24 +51,3 @@ hook::func("privmsg", function($u){
 });
 
 
-hook::func("privmsg", function($u){
-	global $gw,$batch;
-	
-	$nick = $u['nick'];
-	$parv = explode(" ",$u['parc']);
-	$cmd = $parv[0];
-	if ($cmd !== "!raw"){ return; }
-	if ($nick !== "Valware") { return; }
-	$parvs = str_replace($cmd." ","",$u['parc']);
-	$gw->sendraw($parvs);
-});
-
-hook::func("join", function($u){
-	
-	global $gw,$me;
-	
-	$dest = $u['dest'];
-	$nick = $u['nick'];
-	
-	if ($nick !== $me && $dest === "#Valeyard"){ $gw->msg($dest,$u['mtags']); }
-});
