97c7f0ff7422c597b63d84e9628a87b94b2e6cb8
ValwareIRC <v.a.pond@outlook.com>
2021-06-02 01:43:16 +0100

    add commands SAY and ACT
    This module uses WordPress to find a users permission, since this is an integration bot.
    
    These modules are not loaded by default.
    
    If a user has a permission higher than 0, they have permission to make the bot perform SAY and ACT on channels.

---
+++ src/modules/vy_act.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+
+	ACT
+
+	This module provides command ACT
+
+	Permission found on WordPress using usermeta wp_user_level
+	
+	Syntax: ACT #channel <string>
+	
+	Not a fantasy command
+	
+	By Valware
+	
+*/
+
+
+hook::func("privmsg", function($u){
+	
+	global $gw,$me;
+	
+	// split up their params
+	$parv = explode(" ",$u['parc']);
+	
+	// if it was not a privmsg for us, return
+	if ($u['dest'] !== $me){ return; }
+	
+	// if the command is not "ACT", it's not for us, return;
+	if (($cmd = $parv[0]) !== "act"){ return; }
+	
+	// grab nick and transform it to wordpress *_user_level
+	$nick = $u['nick'];
+	
+	// grab anope account if any, if none return
+	if (!($account = account($nick))){ return; }
+	
+	// if account doesn't exist in wordpress, deny
+	if (!($id = WPLogin2ID($account))){ return; }
+	
+	// grab their level
+	$level = WPUserMetaUserLevel($id);
+	
+	// if they have no permissi0ns
+	if ($level < 1){ return; }
+	
+	
+	/* they got permission for this module, let's go */
+	
+	// check if Valeyard is even on that channel
+	$chan = $parv[1];
+	if (!IsOn($me,$chan)){
+		
+		// tell em and return;
+		$gw->notice($nick,"I am not on that channel ($chan).");
+		return;
+	}
+	
+	// act what they want
+	
+	$string = str_replace($parv[0]." ".$parv[1]." ","",$u['parc']);	
+	
+	$gw->act($chan,$string);
+	return;
+
+});
+	
+	
