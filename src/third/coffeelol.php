0201f107d86922b15a773d551b2a16ed57616eeb
ValwareIRC <v.a.pond@outlook.com>
2021-06-10 14:49:05 +0100

    givem the ol switcheroo because I forgot which way round they were fml 😂😂

--- src/third/coffeelol.php
+++ src/third/coffeelol.php
@@ -26,7 +26,7 @@ hook::func("privmsg", function($u){
 
     // if the user does not says "!coffee" ignore it and return safely
     if ($cmd !== "!coffee") { return; }
-    $targ = (isset($parv[1])) : $parv[1] ? $nick;
+    $targ = (isset($parv[1])) ? $parv[1] : $nick;
 
 
     // send the coffee using @+draft/reply=$msgid lmao
